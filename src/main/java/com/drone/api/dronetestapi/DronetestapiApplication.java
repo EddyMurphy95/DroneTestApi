19e57a54a29371f28560d7118d869da6fad461db
Nana Oduro <eddymurphy413@gmail.com>
2022-09-21 07:13:21 +0000

    first commit

---
+++ src/main/java/com/drone/api/dronetestapi/DronetestapiApplication.java
@@ -0,0 +1,13 @@
+package com.drone.api.dronetestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronetestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronetestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drone/api/dronetestapi/DronetestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drone.api.dronetestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronetestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
